b4c7248e37c75c6d75db5448cc50c65e5ec80e64
ilyasassiks <antosinoftenksvov@gmail.com>
2025-03-16 13:50:14 +0300

    .

--- Content.Server/Imperial/Medieval/BellMap/BellMapSystems.cs
+++ Content.Server/Imperial/Medieval/BellMap/BellMapSystems.cs
@@ -1,14 +1,17 @@
+using Content.Server.Mind;
 using Content.Server.Popups;
+using Content.Shared.Coordinates;
 using Content.Shared.Imperial.BellMap.Components;
 using Content.Shared.Interaction;
-using Content.Shared.Popups;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Player;
 
 namespace Content.Server.Imperial.BellMap.Systems;
 
 public sealed partial class BellMapSystem : EntitySystem
 {
     [Dependency] private readonly SharedTransformSystem _transformSystem = default!;
+    [Dependency] private readonly MindSystem _mindSystem = default!;
     [Dependency] private readonly PopupSystem _popupSystem = default!;
     [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
     public override void Initialize()
@@ -19,14 +22,16 @@ public sealed partial class BellMapSystem : EntitySystem
     }
     public void OnUse(EntityUid uid, BellMapComponent comp, InteractHandEvent ev)
     {
-        var mapId = _transformSystem.GetMapId(uid);
-        var query = EntityQueryEnumerator<TransformComponent>();
-        while (query.MoveNext(out var uidd, out var _))
+        var mapId = _transformSystem.GetMapId(ev.Target);
+        var all = _mindSystem.GetAliveHumans();
+        foreach (var mind in all)
         {
-            if (mapId == _transformSystem.GetMapId(uidd))
+            if (mind.Comp.CurrentEntity == null) return;
+            var uidd = mind.Comp.CurrentEntity.Value;
+            if (mapId == _transformSystem.GetMapId(uidd.ToCoordinates()))
             {
-                _popupSystem.PopupEntity(Loc.GetString("bell-map-popup"), uidd, PopupType.MediumCaution);
-                _audioSystem.PlayPvs(comp.Sound, uidd, comp.Params);
+                _popupSystem.PopupEntity(Loc.GetString(comp.Locale), uidd, uidd);
+                _audioSystem.PlayEntity(comp.Sound, Filter.Entities(uidd), uidd, false, comp.Params);
             }
         }
     }
--- Content.Shared/Imperial/Medieval/BellMap/BellMapComponent.cs
+++ Content.Shared/Imperial/Medieval/BellMap/BellMapComponent.cs
@@ -7,7 +7,9 @@ namespace Content.Shared.Imperial.BellMap.Components;
 public sealed partial class BellMapComponent : Component
 {
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public string Sound { get; set; } = "/Audio/Announcements/bell.ogg";
+    public string Sound { get; set; } = "/Audio/Announcements/attention.ogg";
 
     public AudioParams Params = AudioParams.Default.WithVolume(5f);
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public string Locale = "bell-map-popup";
 }
