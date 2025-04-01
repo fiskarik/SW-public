2f4e4a634abbd49216f4261fafffb7690876ed3b
Mona Hmiza <hmizahmizowna@hmiza.hmiza>
2025-04-02 02:34:32 +0300

    ItemDisplaySystem

---
+++ Content.Client/Imperial/Medieval/ItemShow/ItemDisplayOverlay.cs
@@ -0,0 +1,71 @@
+﻿using System.Numerics;
+using Content.Shared.Imperial.Medieval.ItemShow;
+using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
+using Robust.Client.Player;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client.Imperial.Medieval.ItemShow;
+
+public sealed class ItemDisplayOverlay : Overlay
+{
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly IEyeManager _eyeManager = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+
+    private readonly EntityLookupSystem _entityLookupSystem;
+    private readonly SpriteSystem _spriteSystem;
+
+    private ShaderInstance _shader;
+
+    public ItemDisplayOverlay()
+    {
+        IoCManager.InjectDependencies(this);
+
+        _spriteSystem = _entityManager.System<SpriteSystem>();
+        _entityLookupSystem = _entityManager.System<EntityLookupSystem>();
+
+        _shader = _prototypeManager.Index<ShaderPrototype>("unshaded").Instance();
+    }
+
+    protected override void Draw(in OverlayDrawArgs args)
+    {
+        var localPlayer = _playerManager.LocalEntity;
+
+        if (!localPlayer.HasValue)
+        {
+            return;
+        }
+
+        var query = _entityManager.EntityQueryEnumerator<ItemDisplayComponent, TransformComponent, SpriteComponent>();
+
+        while (query.MoveNext(out var uid, out var itemShowComponent, out  var xformComponent, out var spriteComponent))
+        {
+            if (xformComponent.MapID != args.MapId ||
+                !spriteComponent.Visible ||
+                !_entityManager.TryGetComponent<SpriteComponent>(itemShowComponent.ItemUid,
+                    out var itemSpriteComponent))
+            {
+                continue;
+            }
+
+            var currentZoom = _eyeManager.CurrentEye.Scale;
+
+
+            var aabb = _entityLookupSystem.GetWorldAABB(uid);
+
+            var screenCoordinates = _eyeManager.WorldToScreen(aabb.Center +
+                                                              new Angle(-_eyeManager.CurrentEye.Rotation).RotateVec(
+                                                                  aabb.TopRight - aabb.Center));
+
+            _spriteSystem.ForceUpdate(itemShowComponent.ItemUid);
+
+            args.ScreenHandle.UseShader(_shader);
+
+            args.ScreenHandle.DrawEntity(itemShowComponent.ItemUid, screenCoordinates, currentZoom, Angle.Zero, Angle.Zero, Direction.South);
+
+            args.ScreenHandle.UseShader(null);
+        }
+    }
+}
---
+++ Content.Server/Imperial/Medieval/ItemShow/ItemDisplaySystem.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using Content.Server.Hands.Systems;
+using Content.Shared.Hands;
+using Content.Shared.Imperial.Medieval.ItemShow;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Throwing;
+using Robust.Shared.Timing;
+
+namespace Content.Server.Imperial.Medieval.ItemShow;
+
+public sealed class ItemDisplaySystem : EntitySystem
+{
+    [Dependency] private readonly HandsSystem _handsSystem = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+
+    public override void Initialize()
+    {
+        SubscribeNetworkEvent<ItemDisplayRequest>(OnItemShowRequest);
+        SubscribeLocalEvent<ItemDisplayComponent, DidUnequipHandEvent>(OnUnequipped);
+    }
+
+    private void OnUnequipped(Entity<ItemDisplayComponent> ent, ref DidUnequipHandEvent args)
+    {
+        if (ent.Comp.ItemUid != args.Unequipped)
+        {
+            return;
+        }
+
+        RemComp<ItemDisplayComponent>(ent);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<ItemDisplayComponent>();
+
+        while (query.MoveNext(out var uid, out var itemShowComponent))
+        {
+            if (itemShowComponent.DespawnAt >= _gameTiming.CurTime)
+            {
+                continue;
+            }
+
+            RemComp<ItemDisplayComponent>(uid);
+        }
+    }
+
+    private void OnItemShowRequest(ItemDisplayRequest msg, EntitySessionEventArgs args)
+    {
+        var player = args.SenderSession.AttachedEntity;
+
+        if (player == null)
+        {
+            Log.Error($"Attempt to show an item failed: no valid player entity found for {args.SenderSession.UserId}.");
+            return;
+        }
+
+        if (!TryGetEntity(msg.ItemUid, out var itemUid))
+        {
+            Log.Error($"Attempt to show an item failed: provided entity UID is invalid {msg.ItemUid}.");
+            return;
+        }
+
+        if (_handsSystem.EnumerateHeld(player.Value).All(x => x != itemUid))
+        {
+            Log.Error($"Attempt to show an item failed: player does not hold the specified item {itemUid}.");
+            return;
+        }
+
+        var comp = EnsureComp<ItemDisplayComponent>(player.Value);
+
+        comp.ItemUid = itemUid.Value;
+        comp.DespawnAt = _gameTiming.CurTime + comp.DespawnDelay;
+
+        Dirty(player.Value, comp);
+    }
+}
