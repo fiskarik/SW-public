c573d78f9471d773fb18a4e20a15c191012c0d7f
The_Fiafi <thewaffelfiafi@gmail.com>
2025-05-02 19:33:00 +0300

    feat: flinter

---
+++ Content.Server/Imperial/Medieval/FlammableExpendableLight/FlammableExpendableLightSystem.cs
@@ -0,0 +1,63 @@
+using Content.Server.Light.Components;
+using Content.Server.Light.EntitySystems;
+using Content.Shared.Imperial.Medieval.Igniter;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Temperature;
+
+namespace Content.Server.Imperial.Medieval.FlammableExpendableLight;
+
+
+public sealed partial class FlammableExpendableLightSystem : EntitySystem
+{
+    [Dependency] private readonly ExpendableLightSystem _expendableLightSystem = default!;
+
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<FlammableExpendableLightComponent, IgniteEvent>(OnIgnite);
+        SubscribeLocalEvent<FlammableExpendableLightComponent, AfterInteractEvent>(OnAfterInteract);
+        SubscribeLocalEvent<FlammableExpendableLightComponent, AfterInteractUsingEvent>(OnAfterInteractUsing);
+
+        SubscribeLocalEvent<FlammableExpendableLightComponent, UseInHandEvent>(OnActivate, before: [typeof(ExpendableLightSystem)]);
+    }
+
+    private void OnIgnite(EntityUid uid, FlammableExpendableLightComponent component, IgniteEvent args)
+    {
+        if (!TryComp<ExpendableLightComponent>(uid, out var expendableLightComponent)) return;
+
+        _expendableLightSystem.TryActivate((uid, expendableLightComponent));
+    }
+
+    private void OnAfterInteract(EntityUid uid, FlammableExpendableLightComponent component, AfterInteractEvent args)
+    {
+        if (args.Target == null) return;
+        if (!TryComp<ExpendableLightComponent>(uid, out var expendableLightComponent)) return;
+
+        var isHotEvent = new IsHotEvent();
+        RaiseLocalEvent(args.Target.Value, isHotEvent, false);
+
+        if (!isHotEvent.IsHot) return;
+
+        _expendableLightSystem.TryActivate((uid, expendableLightComponent));
+    }
+
+    private void OnAfterInteractUsing(EntityUid uid, FlammableExpendableLightComponent component, AfterInteractUsingEvent args)
+    {
+        if (!TryComp<ExpendableLightComponent>(uid, out var expendableLightComponent)) return;
+
+        var isHotEvent = new IsHotEvent();
+        RaiseLocalEvent(args.Used, isHotEvent, false);
+
+        if (!isHotEvent.IsHot) return;
+
+        _expendableLightSystem.TryActivate((uid, expendableLightComponent));
+    }
+
+    private void OnActivate(EntityUid uid, FlammableExpendableLightComponent component, UseInHandEvent args)
+    {
+        args.Handled = true;
+    }
+}
